334da45be11bfc53ca76af358c90e38a1ef9b28b
hasibrozi <hasib.rozi@gmail.com>
2023-03-07 17:49:42 +0700

    Delete Parameters

--- cypress/e2e/3-api/get-list-users.cy.js
+++ cypress/e2e/3-api/get-list-users.cy.js
@@ -4,7 +4,7 @@ describe('Get Users', () => {
     it('Verify the users list will displayed', () => {
         cy.request({
             method: "GET",
-            url: 'https://reqres.in/api/users?page=2&per_pages=1&delay=3',
+            url: 'https://reqres.in/api/users',
         }).as('users')
         cy.get('@users').its('status').should('equal', 200)
     })
